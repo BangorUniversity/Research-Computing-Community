93f85e16eba2c2af71d21616e697839285087a11
issa16 <issa16@bangor.ac.uk>
2020-01-07 19:18:48 +0000

    Added references

---
+++ tutorials/cpp/beginner/05_references/main.cpp
@@ -0,0 +1,36 @@
+#include <exception>
+#include <iostream>
+
+void print_num(const double& i) { std::cout << i << "\n"; }
+
+int main(int argc, char* argv[]) try {
+  int a = 1;
+  std::cout << "a = " << a << "\n";
+
+  int& b = a;
+  std::cout << "b = " << b << "\n\n";
+
+  double c = 1.2;
+  std::cout << "c = " << c << "\n";
+
+  double& d = c;
+  std::cout << "d = " << c << "\n\n";
+
+  c = 3.4;
+  std::cout << "c = " << c << "\n";
+  std::cout << "d = " << d << "\n\n";
+
+  d = 5.6;
+  std::cout << "c = " << c << "\n";
+  std::cout << "d = " << d << "\n\n";
+
+  print_num(d);
+
+  return 0;
+} catch (const std::exception& e) {
+  std::cerr << e.what() << "\n";
+  return 1;
+} catch (...) {
+  std::cerr << "An exception occurred.\n";
+  return 2;
+}
