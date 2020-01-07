34cce503999e763920b211df358bc8659fd32d8f
issa16 <issa16@bangor.ac.uk>
2020-01-07 18:59:08 +0000

    Added pointers

---
+++ tutorials/cpp/beginner/04_pointers/main.cpp
@@ -0,0 +1,30 @@
+#include <exception>
+#include <iostream>
+
+int main(int argc, char* argv[]) try {
+  int a = 1;
+  std::cout << "a = " << a << "\n";
+  std::cout << "The address of 'a' is " << &a << "\n";
+
+  int* b = &a;
+  std::cout << "b = " << b << "\n";
+  std::cout << "The contents of address " << b << " is " << *b << "\n\n";
+
+  char c = 'x';
+  std::cout << "c = " << c << "\n";
+  std::cout << "The address of 'c' is " << static_cast<void*>(&c) << "\n";
+
+  char* d = &c;
+  std::cout << "d = " << static_cast<void*>(d) << "\n";
+  std::cout << "The contents of address " << static_cast<void*>(d) << " is " << *d << "\n";
+
+  double* e = nullptr;
+
+  return 0;
+} catch (const std::exception& e) {
+  std::cerr << e.what() << "\n";
+  return 1;
+} catch (...) {
+  std::cerr << "An exception occurred.\n";
+  return 2;
+}
