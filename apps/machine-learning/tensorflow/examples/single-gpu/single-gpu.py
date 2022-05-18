e448530f6adaefb78f5c22cba5087b6d5226cc78
issa16 <issa16@bangor.ac.uk>
2022-05-18 11:41:31 +0100

    Update examples

--- apps/machine-learning/tensorflow/examples/single-gpu/single-gpu.py
+++ apps/machine-learning/tensorflow/examples/single-gpu/single-gpu.py
@@ -8,18 +8,22 @@ tf.config.threading.set_inter_op_parallelism_threads(num_threads)
 tf.config.threading.set_intra_op_parallelism_threads(num_threads)
 tf.config.set_soft_device_placement(True)
 
-print('*' * 80)
 parallel_threads = tf.config.threading.get_inter_op_parallelism_threads()
-print(f'Num threads: {parallel_threads}')
+print(
+    f'Number of threads used for parallelism between independent operations.: {parallel_threads}'
+)
 
-print('*' * 80)
+parallel_threads = tf.config.threading.get_intra_op_parallelism_threads()
+print(
+    f'Get number of threads used within an individual op for parallelism.: {parallel_threads}'
+)
+
 logical_devices = tf.config.list_logical_devices('GPU')
 print(f'Logical Num GPUs: {len(logical_devices)}')
-for devices in logical_devices:
-    print(devices)
+for device in logical_devices:
+    print(f'\t {device}')
 
-print('*' * 80)
 physical_devices = tf.config.list_physical_devices('GPU')
 print(f'Physical Num GPUs: {len(logical_devices)}')
-for devices in physical_devices:
-    print(devices)
+for device in physical_devices:
+    print(f'\t {device}')
