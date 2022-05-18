34f53940a8d1e23d09275743a6ee1e35debb9827
issa16 <issa16@bangor.ac.uk>
2022-05-18 09:57:22 +0100

    Added thread count to examples

--- apps/machine-learning/tensorflow/examples/multi-cpu/multi-cpu.py
+++ apps/machine-learning/tensorflow/examples/multi-cpu/multi-cpu.py
@@ -1,13 +1,17 @@
 import tensorflow as tf
 
 print('*' * 80)
+num_threads = tf.config.threading.get_inter_op_parallelism_threads()
+print(f'Num threads: {num_threads}')
+
+print('*' * 80)
 logical_devices = tf.config.list_logical_devices('CPU')
-print("Logical Num CPUs:", len(logical_devices))
+print(f'Logical Num CPUs: {len(logical_devices)}')
 for devices in logical_devices:
     print(devices)
 
 print('*' * 80)
 physical_devices = tf.config.list_physical_devices('CPU')
-print("Physical Num CPUs:", len(physical_devices))
+print(f'Physical Num CPUs: {len(logical_devices)}')
 for devices in physical_devices:
     print(devices)
--- apps/machine-learning/tensorflow/examples/single-gpu/single-gpu.py
+++ apps/machine-learning/tensorflow/examples/single-gpu/single-gpu.py
@@ -1,13 +1,17 @@
 import tensorflow as tf
 
 print('*' * 80)
+num_threads = tf.config.threading.get_inter_op_parallelism_threads()
+print(f'Num threads: {num_threads}')
+
+print('*' * 80)
 logical_devices = tf.config.list_logical_devices('GPU')
-print("Logical Num GPUs:", len(logical_devices))
+print(f'Logical Num GPUs: {len(logical_devices)}')
 for devices in logical_devices:
     print(devices)
 
 print('*' * 80)
 physical_devices = tf.config.list_physical_devices('GPU')
-print("Physical Num GPUs:", len(physical_devices))
+print(f'Physical Num GPUs: {len(logical_devices)}')
 for devices in physical_devices:
     print(devices)
