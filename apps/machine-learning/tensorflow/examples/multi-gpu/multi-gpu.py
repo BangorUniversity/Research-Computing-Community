b02c77d37803a71f12fb3d95e9b69f6863dbae75
issa16 <issa16@bangor.ac.uk>
2022-05-18 09:44:39 +0100

    Updated examples

--- apps/machine-learning/tensorflow/examples/multi-gpu/multi-gpu.py
+++ apps/machine-learning/tensorflow/examples/multi-gpu/multi-gpu.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
 
+print('*' * 80)
+logical_devices = tf.config.list_logical_devices('GPU')
+print("Logical Num GPUs:", len(logical_devices))
+for devices in logical_devices:
+    print(devices)
+
+print('*' * 80)
 physical_devices = tf.config.list_physical_devices('GPU')
-
-print("Num GPUs:", len(physical_devices))
-
+print("Physical Num GPUs:", len(physical_devices))
 for devices in physical_devices:
     print(devices)
--- apps/machine-learning/tensorflow/examples/single-cpu/single-cpu.py
+++ apps/machine-learning/tensorflow/examples/single-cpu/single-cpu.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
 
+print('*' * 80)
+logical_devices = tf.config.list_logical_devices('CPU')
+print("Logical Num CPUs:", len(logical_devices))
+for devices in logical_devices:
+    print(devices)
+
+print('*' * 80)
 physical_devices = tf.config.list_physical_devices('CPU')
-
-print("Num CPUs:", len(physical_devices))
-
+print("Physical Num CPUs:", len(physical_devices))
 for devices in physical_devices:
     print(devices)
